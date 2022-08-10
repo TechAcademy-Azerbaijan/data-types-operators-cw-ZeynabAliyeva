9df82e67e9ab287840520f2c1d667d1a6330d19b
ZeynabAliyeva <103027531+ZeynabAliyeva@users.noreply.github.com>
2022-08-11 03:46:49 +0400

    task-1 clean

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let input = result.input;
+    let n = input.split(',');
+    let a = parseInt(n[0]);
+    let b = parseInt(n[1]);
+    let c = parseInt(n[2]);
+    let cvb = a**b ;
+    console.log(cvb);
   
 });
