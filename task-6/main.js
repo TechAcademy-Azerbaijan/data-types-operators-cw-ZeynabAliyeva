dea782dfca098c1965c5a2a655006c575b6ab3c6
ZeynabAliyeva <103027531+ZeynabAliyeva@users.noreply.github.com>
2022-08-11 03:50:11 +0400

    task6 clean

--- task-6/main.js
+++ task-6/main.js
@@ -3,11 +3,8 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    var input = result.input;
-    var input_data = input.split(',');
-    var n = parseInt(input_data[0]);
-    var massiv = input_data[1].split(' ');
-    console.log(input_data);
-  
+ let n = result.input; 
+ let cvb = parseInt(n/10);
+ console.log(cvb);
+
 });
